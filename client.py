1e85250a2debf886002de8bbe3afbc71e3d0f768
CarterWS <47007986+CarterWS@users.noreply.github.com>
2020-06-29 13:52:12 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -7,8 +7,7 @@ import bluetooth
 import time
 import zerorpc
 
-#Pyboard = 48:4A:30:01:80:B2, CEEO EV3 = F0:45:DA:15:FB:8D, My EV3 = A0:E6:F8:60:17:5E
-serverMACAddress = 'F0:45:DA:15:FB:8D' #change to your EV3 Bluetooth address
+serverMACAddress = '' #change to your EV3 Bluetooth address
 port = 3
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
